01daf7d4332688c8641a7ca3b1390bc51334617e
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-01-04 05:24:54 +0000

    Added setup.py

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # PySimpleSockets
-A simple networking library for python
+## A simple networking library for python
+---
+This library uses the in-built `sockets` library
+
+It also uses the most basic client-server model
+
+To initialize a server:
+```python
+from pySimpleSockets import Server
+
+myServer = Server(
+    IP,
+    PORT,
+    FORMAT='utf-8',
+    DISCONNECT='!Disconnect'
+)
+myServer.start()
+
+def callback(conn, addr, *args):
+    print(f"({addr}) connected")
+
+def disconnect(conn, addr, *args):
+    print(f"({addr}) disconnected")
+
+myServer.onConnect(callback, args=(), kwargs={})
+myServer.onDicconnect(disconnect, args=(), kwargs={})
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="PySimpleSockets",
+    version="0.0.1",
+    author="DrSparky-2007",
+    author_email="adityaiyer2007@gmail.com",
+    description="A simple networking library for python",
+    long_description=l_desc,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrSparky-2007/PySimpleSockets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keys=[
+        'networking',
+        'sockets',
+        'simple networking',
+        'simple sockets'
+    ],
+    python_requires='>=3.6'
+)
