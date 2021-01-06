0984a1f8885a1bc828281716bee73d04bc3bd179
DrSparky-2007 <adityaiyer2007@gmail.com>
2021-01-06 04:37:32 +0000

    Implement message sending

--- pySimpleSockets/__init__.py
+++ pySimpleSockets/__init__.py
@@ -39,17 +39,17 @@ class Server(BaseSocket):
         for callback in self.messageCallbacks:
             callback(addr, conn, msg)
     
-    def onConnect(self, callback, args, kwargs):
+    def onConnect(self, callback, args:tuple = (), kwargs:dict = {}) -> None:
         self.connectCallbacks.append(
             lambda addr, conn: \
             callback(addr, conn, *args, **kwargs)
         )
-    def onDicconnect(self, callback, args, kwargs):
+    def onDicconnect(self, callback, args:tuple = (), kwargs:dict = {}) -> None:
         self.disconnectCallbacks.append(
             lambda addr: \
             callback(addr, *args, **kwargs)
         )
-    def onMessage(self, callback, args, kwargs):
+    def onMessage(self, callback, args:tuple = (), kwargs:dict = {}) -> None:
         self.messageCallbacks.append(
             lambda addr, conn, msg: \
             callback(addr, conn, msg, *args, **kwargs)
@@ -64,13 +64,9 @@ class Server(BaseSocket):
     def __client_thread(self, addr, conn):
         clientConnected = True
         while self.running and clientConnected:
-            msg_size = int(\
-                self.server.recv(self.HEADER)\
-                .decode(self.FORMAT)
-                )
-            msg = self.server.recv(msg)
+            msgLen = int(conn.recv(self.HEADER).decode(self.FORMAT))
+            msg = conn.recv(msgLen).decode(self.FORMAT)
             self._onMessageCallback(addr, conn, msg)
-    
     def __handle_thread_start(self):
         import threading
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -80,10 +76,9 @@ class Server(BaseSocket):
         while self.running:
             conn, addr = self.server.accept()
             self._onConnectCallback(addr, conn)
-            print("Connection!")
+            print("[SERVER]\tConnection!")
             clientThread = threading.Thread(target=self.__client_thread, args=(addr, conn))
             clientThread.start()
-    
     def start(self, onThread = True):
         if onThread:
             import threading
@@ -101,8 +96,63 @@ class Client(BaseSocket):
             FORMAT='utf-8',
             DISCONNECT='!Disconnect'):
         super().__init__(IP, PORT, HEADER, FORMAT, DISCONNECT)
+        self.messageCallbacks = []
+        self.client = None
+        self.connected = False
+    
+    def _onMessageCallback(self, msg):
+        for callback in self.messageCallbacks:
+            callback(msg)
+    def onMessage(self, callback, args:tuple = (), kwargs:dict = {}) -> None:
+        self.messageCallbacks.append(
+            lambda msg:\
+            callback(msg, *args, **kwargs)
+        )
+    def send(self, message):
+        if self.client != None:
+            msgLen = str(len(message))
+            sendMsgLen = (msgLen + ' ' * (self.HEADER - len(msgLen))).encode(self.FORMAT)
+            self.client.send(sendMsgLen)
+            self.client.send(message.encode(self.FORMAT))
+        else:
+            raise ValueError("Can't send a message without connecting to a server")
+    
+    def __client_listen(self):
+        while self.connected:
+            msgLen = int(self.client.recv(self.HEADER).decode(self.FORMAT))
+            msg = self.client.recv(msgLen).decode(self.FORMAT)
+            self._onMessageCallback(msg)
+    def __handle_thread_connect(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.ADDR)
+    def connect(self, onThread = True):
+        self.connected = True
+        if onThread:
+            import threading
+            thread = threading.Thread(
+                target = self.__handle_thread_connect
+            )
+            thread.start()
+        else:
+            self.__handle_thread_connect()
 
 if __name__ == '__main__':
-    svr = Server(socket.gethostbyname(socket.gethostname()), 6050, 16)
-    print(svr.ADDR)
-    svr.start()
+    def startServerTest():
+        svr = Server(socket.gethostbyname(socket.gethostname()), 6050, 16)
+        svr.onConnect(lambda addr, conn, *_, **__: print("[CLIENT]\t" + str(addr)))
+        svr.onConnect(lambda addr, conn: svr.sendTo(conn, "Hello from server!"))
+        svr.onMessage(
+            lambda addr, conn, msg: print("[MESSAGE]{" + str(addr) + "}\t" + msg)
+        )
+        svr.start()
+        print("[SERVER]\t" + str(svr.ADDR))
+    def startClientTest():
+        client = Client(socket.gethostbyname(socket.gethostname()), 6050, 16)
+        client.onMessage(lambda msg, *_, **__: print("[MESSAGE]{SERVER}\t" + msg))
+        print(client.messageCallbacks)
+        client.connect()
+        while client.client == None:
+            pass
+        client.send("Hello")
+    startServerTest()
+    startClientTest()
