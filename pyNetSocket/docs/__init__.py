6d770fcc49b564783704dfcfdcc81c1a826511a4
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2021-01-09 13:39:26 +0530

    Update __init__.py

--- pyNetSocket/docs/__init__.py
+++ pyNetSocket/docs/__init__.py
@@ -2,5 +2,9 @@ print('The documentation for the pyNetSocket library')
 print('This covers all the information you need to start')
 print('')
 print('You can see the full guides here:')
-print('https://github.com/DrSparky-2007/PyNetSocket/')
-openLink = input('Open link in browser? (y/n) ')
+print('https://github.com/DrSparky-2007/PyNetSocket/wiki/')
+openLink = input('Open link in browser? (y/n) ')[0].lower()
+
+if openLink == 'y':
+  import webbrowser as wb
+  wb.open('https://github.com/DrSparky-2007/PyNetSocket/wiki/')
