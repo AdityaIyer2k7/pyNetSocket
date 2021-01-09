362ae6fc22b98b1b5a52a8b9020646f0ee92bd06
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2021-01-09 15:20:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as fl:
 
 setuptools.setup(
     name="pyNetSockets",
-    version="1.0.2",
+    version="1.0.3",
     author="DrSparky-2007",
     author_email="adityaiyer2007@gmail.com",
     description="A simple networking library for python",
