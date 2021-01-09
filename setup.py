15d80ee2f230a4601cbc4a44011bf8de27592bf0
DrSparky-2007 <56183042+DrSparky-2007@users.noreply.github.com>
2021-01-09 12:45:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A simple networking library for python",
     long_description=l_desc,
     long_description_content_type="text/markdown",
-    url="https://github.com/DrSparky-2007/PySimpleSockets",
+    url="https://github.com/DrSparky-2007/PyNetSocket",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
