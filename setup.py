43cac3b0bf0179a3ca2146f07b2e17eb5e766a3e
DrSparky2k7 <56183042+DrSparky2k7@users.noreply.github.com>
2021-05-13 19:56:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('README.md') as fl:
 
 setuptools.setup(
     name="pyNetSocket",
-    version="1.1.2",
-    author="DrSparky-2007",
+    version="1.1.3",
+    author="DrSparky2k7",
     author_email="adityaiyer2007@gmail.com",
     description="A simple networking library for python",
     long_description=l_desc,
     long_description_content_type="text/markdown",
-    url="https://github.com/DrSparky-2007/PyNetSocket",
+    url="https://github.com/DrSparky2k7/PyNetSocket",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
